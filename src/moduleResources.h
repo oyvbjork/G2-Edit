9aafb3e60399b318f29bd326d4deb83591c9692e
Øyvind Jacobsen Bjørkås <oyvind@bjorkas.no>
2025-06-28 17:40:59 +0200

    ValSw1-2 and ValSw2-1

--- src/moduleResources.h
+++ src/moduleResources.h
@@ -33,6 +33,7 @@ const char * emptyStrMap[]            = {" ", NULL};
 const char * driverTypeStrMap[]       = {"Reed", "Bow", "-Lip-", "-Mallet-", NULL};
 const char * octaveStrMap[]           = {"C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", NULL};
 const char * bip128StrMap[]           = {"-64", "-63", "-62", "-61", "-60", "-59", "-58", "-57", "-56", "-55", "-54", "-53", "-52", "-51", "-50", "-49", "-48", "-47", "-46", "-45", "-44", "-43", "-42", "-41", "-40", "-39", "-38", "-37", "-36", "-35", "-34", "-33", "-32", "-31", "-30", "-29", "-28", "-27", "-26", "-25", "-24", "-23", "-22", "-21", "-20", "-19", "-18", "-17", "-16", "-15", "-14", "-13", "-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", NULL};
+const char * u128StrMap[]             = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127"};
 const char * captureStrMap[]          = {"Closest", "Evenly", NULL};
 const char * fltNordDbStrMap[]        = {"12dB", "24dB", NULL};
 const char * offTo100KbStrMap[]       = {"Off", "25%", "50%", "75%", "100%", NULL};
@@ -377,7 +378,7 @@ const tModuleProperties gModuleProperties[] = {
 // moduleType, paramType1, paramType2, position rectangle, anchor, label, range, defaultValue, string map, colour map
 const tParamLocation paramLocationList[] = {
     //  0 Unknown
-    //  1 Keyboard
+    //  1 Keyboard -- No params
     //  2 Unknown
     //  3 4-Out
     {moduleType4toOut,     paramType1StandardToggle, paramType2Toggle, {{ 25,  -3}, { 7,  7}}, anchorBottomLeft,  "Out to",         3,   0, outTo4OutStrMap,        NULL          }, // 3 4-Out
@@ -441,6 +442,7 @@ const tParamLocation paramLocationList[] = {
     // 15 Sw8-1 *** Needs more resources
     // 16 Unknown
     // 17 ValSw1-2 *** Could need more resources
+    {moduleTypeValSw1to2,    paramType1UpDown,     paramType2UpDown,   {{ 40,  -3}, { 7, 14}}, anchorBottomLeft,  NULL,           128,   0, u128StrMap,                   NULL          }, // 17 Val
     // 18 X-Fade
     {moduleTypeXtoFade,    paramType1CommonDial,     paramType2Dial,   {{ 40,  -3}, { 7, 14}}, anchorBottomLeft,  NULL,           128,   0, NULL,                   NULL          }, // 18 Mod
     {moduleTypeXtoFade,    paramType1Pan,            paramType2Dial,   {{ 55,   4}, { 7, 14}}, anchorBottomLeft,  NULL,           128,  64, NULL,                   NULL          }, // 18 X-fade
@@ -850,7 +852,7 @@ const tParamLocation paramLocationList[] = {
 
     // 104 Unknown
     // 105 ValSw2-1
-    {moduleTypeValSw2to1,  paramType1CommonDial,     paramType2Dial,   {{ 40,  -3}, { 7, 14}}, anchorBottomLeft,  "Ctrl Value",    64,   0, NULL,                   NULL          }, // 105 Value
+    {moduleTypeValSw2to1,    paramType1UpDown,     paramType2UpDown,   {{ 40,  -3}, { 7, 14}}, anchorBottomLeft,  NULL,           128,   0, u128StrMap,                   NULL          }, // 105 Val
     // 106 OscNoise
     {moduleTypeOscNoise,   paramType1Freq,           paramType2Dial,   {{ 32,  -3}, { 7, 14}}, anchorBottomLeft,  "Tune",         128,  64, NULL,                   NULL          }, // 106 Tune
     {moduleTypeOscNoise,   paramType1CommonDial,     paramType2Dial,   {{ 45,  -3}, { 7, 14}}, anchorBottomLeft,  "Cent",         128,  64, NULL,                   NULL          }, // 106 Fine
