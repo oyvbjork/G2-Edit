168c2d91451ab7f2f9b5c63fa93c655bba8f7f7a
Chris Turner <chris_purusha@icloud.com>
2025-07-01 12:05:38 +0100

    Rename of the new source files, to match the others.

--- src/renderParams.h
+++ src/renderParams.h
@@ -1,9 +1,23 @@
-//
-//  render_params.h
-//  G2-edit
-//
+/*
+ * The G2 Editor application.
+ *
+ * Copyright (C) 2025 Chris Turner <chris_purusha@icloud.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 //  Created by Øyvind Jacobsen Bjørkås on 25/06/2025.
-//
 
 #ifndef __RENDER_PARAMS_H__
 #define __RENDER_PARAMS_H__
