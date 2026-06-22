c81f27c936c3d493700953d2bab8dfbc2daf5b48
Chris Turner <chris_purusha@icloud.com>
2026-06-22 14:26:54 +0100

    Temporary mechanism to reduce stack use and overflow crash

--- src/usbComms.c
+++ src/usbComms.c
@@ -589,12 +589,12 @@ static int parse_patch_version(uint8_t * buff, int length) {
 }
 
 static void parse_param_change(uint32_t slot, uint8_t * buff, int length) {
-    uint32_t   bitPos    = 0;
-    tModule    module    = {0};
-    tModuleKey key       = {0};
-    uint32_t   param     = 0;
-    uint32_t   variation = 0;
-    uint32_t   value     = 0;
+    static tModule module;  // TODO - Temporary measure, since we were over-running the stack! ultimately, malloc the params on a module
+    uint32_t       bitPos    = 0;
+    tModuleKey     key       = {0};
+    uint32_t       param     = 0;
+    uint32_t       variation = 0;
+    uint32_t       value     = 0;
 
     key.slot                             = slot;
     key.location                         = read_bit_stream(buff, &bitPos, 8);
@@ -614,10 +614,10 @@ static void parse_param_change(uint32_t slot, uint8_t * buff, int length) {
 static int parse_command_response(uint8_t * buff, uint32_t * bitPos,
                                   uint8_t commandResponse, uint8_t subCommand,
                                   int length) {
-    tModule  module        = {0};
-    uint32_t slot          = commandResponse & 0x03;
-    int      i             = 0;
-    int      volumesToRead = 0;
+    static tModule module; // TODO - Temporary measure, since we were over-running the stack! ultimately, malloc the params on a module
+    uint32_t       slot          = commandResponse & 0x03;
+    int            i             = 0;
+    int            volumesToRead = 0;
 
     switch (subCommand) {
         case SUB_RESPONSE_VOLUME_INDICATOR:
